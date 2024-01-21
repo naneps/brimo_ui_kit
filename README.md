db752a2c7afe0cad9e87dbbfc0233e199ad7319e
naneps <ekaprasetya2244@gmail.com>
2024-01-21 18:04:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# brimo_ui_kit
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/brimo_ui_kit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.brimo_ui_kit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
